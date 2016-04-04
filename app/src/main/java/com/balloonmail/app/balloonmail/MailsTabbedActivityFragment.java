15c4093b5448dbbfd33b038de069a48186232cd3
DaliaAyman <dalia.ay.ahmed@gmail.com>
2016-04-04 23:48:07 +0200

    MailsTabbedActivity Initial Commit

---
+++ app/src/main/java/com/balloonmail/app/balloonmail/MailsTabbedActivityFragment.java
@@ -0,0 +1,22 @@
+package com.balloonmail.app.balloonmail;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MailsTabbedActivityFragment extends Fragment {
+
+    public MailsTabbedActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mails_tabbed, container, false);
+    }
+}
--- app/src/main/java/com/balloonmail/app/balloonmail/MainActivity.java
+++ app/src/main/java/com/balloonmail/app/balloonmail/MainActivity.java
@@ -1,13 +1,15 @@
 package com.balloonmail.app.balloonmail;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button sentButton = (Button)findViewById(R.id.sent_button);
+        sentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MailsTabbedActivity.class);
+                startActivity(intent);
             }
         });
     }
