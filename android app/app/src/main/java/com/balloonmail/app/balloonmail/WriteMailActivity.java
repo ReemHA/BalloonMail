c7ea8b0c28ad78120b8b315dec6ee32bdfa8aa1a
Reem Abdelrazek <reem.elrazek@gmail.com>
2016-04-24 13:33:15 +0200

    sending mail button is set without using Volley

---
+++ android app/app/src/main/java/com/balloonmail/app/balloonmail/WriteMailActivity.java
@@ -0,0 +1,13 @@
+package com.balloonmail.app.balloonmail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WriteMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_mail);
+    }
+}
