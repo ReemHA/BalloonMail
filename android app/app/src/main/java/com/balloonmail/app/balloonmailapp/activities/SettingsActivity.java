1a27f5eba133096377b8ca83b9d7067a7e393b39
DaliaAyman <dalia.ay.ahmed@gmail.com>
2017-04-30 22:19:47 +0200

    removed dialog of invite preference

--- android app/app/src/main/java/com/balloonmail/app/balloonmailapp/activities/SettingsActivity.java
+++ android app/app/src/main/java/com/balloonmail/app/balloonmailapp/activities/SettingsActivity.java
@@ -3,18 +3,23 @@ package com.balloonmail.app.balloonmailapp.activities;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
 
 import com.balloonmail.app.balloonmailapp.R;
+import com.google.android.gms.appinvite.AppInviteInvitation;
 
 import java.util.List;
 
 public class SettingsActivity extends AppCompatPreferenceActivity {
+    private static final int REQUEST_INVITE = 2;
 
     private static boolean isXLargeTablet(Context context) {
         return (context.getResources().getConfiguration().screenLayout
@@ -57,6 +62,22 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            Preference myPref = (Preference) findPreference("invite");
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                public boolean onPreferenceClick(Preference preference) {
+                    //open browser or intent here
+                    Intent intent = new AppInviteInvitation.IntentBuilder(
+                            getResources().getString(R.string.invitation_title))
+                            .setMessage(getResources().getString(R.string.invitation_message))
+                            .setDeepLink(Uri.parse(getResources().getString(R.string.invitation_deep_link)))
+                            .setCustomImage(Uri.parse(getResources().getString(R.string.invitation_custom_image)))
+                            .setCallToActionText(getResources().getString(R.string.invitation_cta))
+                            .build();
+                    startActivityForResult(intent, REQUEST_INVITE);
+                    return true;
+                }
+            });
         }
     }
 
